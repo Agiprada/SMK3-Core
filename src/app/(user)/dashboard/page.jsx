0df486dab0c92974cb676098dbae5146d3dc7ae6
Agiprada <shintarogilang@gmail.com>
2024-09-04 20:30:27 +0800

    first commit

---
+++ src/app/(user)/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../shared/header'
+import Footer from '../shared/footer'
+
+export default function Dashboard() {
+  return (
+    <div>
+        <Header />
+        <p>
+            Dashboard
+        </p>
+        <Footer/>
+    </div>
+  )
+}
